ebe190a7ffa2dfb27aedb0cbce36a46a25604b93
hulya <hulyakarakayaa06@gmail.com>
2020-11-24 14:25:53 -0800

    configure enzyme and enzyme adapter to create  snapshot test

--- src/components/Input/index.test.js
+++ src/components/Input/index.test.js
@@ -1,13 +1,35 @@
-import {render} from '@testing-library/react'
+import {shallow} from 'enzyme'
 import Input from '.'
+
+const render = (props) => shallow(<Input {...props}></Input>)
 
 describe("Input component", () => {
     let suit;
     let props;
 
-    it("should match snapshot", () => {
-        suit = render(<Input {...props} />)
-        expect(suit).toMatchSnapshot();
+    describe("without props", () => {
+        beforeEach(() => {
+            suit = render(props)
+        })
 
+        it("should match snapshot", () => {
+            // suit = render(<Input {...props} />)
+            expect(suit).toMatchSnapshot();
+        })
     })
+
+    describe('with props', () => {
+        beforeEach(() => {
+            props = {
+                placeholder: 'Input your data',
+                name: 'search'
+            };
+
+            suit = render(props);
+        });
+
+        it('should match snapshot', () => {
+            expect(suit).toMatchSnapshot();
+        })
+    });
 })
