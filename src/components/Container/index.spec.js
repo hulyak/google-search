e073dcb552d0546acfa9f78454efa019b32b26ff
hulya <hulyakarakayaa06@gmail.com>
2020-11-25 21:49:10 -0800

    add tests for Container

--- src/components/Container/index.spec.js
+++ src/components/Container/index.spec.js
@@ -1,47 +1,47 @@
-// import {mount} from 'enzyme'
-// import Container from ".";
+import {mount} from 'enzyme'
+import Container from ".";
 
-// jest.mock('../../hooks', () => ({
-//     useDebounce: jest.fn(() => ({})),
-//     useSearch: jest.fn(() => ({articles: []})),
-//     useSearchValue: jest.fn(() => ({
-//         searchValue: 'testValue',
-//         onSearchChange: jest.fn()
-//     }))
-// }));
+jest.mock('../../hooks', () => ({
+    useDebounce: jest.fn(() => ({})),
+    useSearch: jest.fn(() => ({articles: []})),
+    useSearchValue: jest.fn(() => ({
+        searchValue: 'testValue',
+        onSearchChange: jest.fn()
+    }))
+}));
 
 
-// const render = (props, children) => mount(<Container {...props}>{children}</Container>);
+const render = (props, children) => mount(<Container {...props}>{children}</Container>);
 
-// // children is a function receives three props
-// const Children = () => <div>Test</div>
+// children is a function receives three props
+const Children = () => <div>Test</div>
 
-// describe('Container component', () => {
-//     let sut;
-//     let props;
-//     let children = ({searchValue, onSearchChange, articles}) => <Children searchValue={searchValue}
-//                                                                           onSearchChange={onSearchChange}
-//                                                                           articles={articles}/>;
+describe('Container component', () => {
+    let sut;
+    let props;
+    let children = ({searchValue, onSearchChange, articles}) => <Children searchValue={searchValue}
+                                                                          onSearchChange={onSearchChange}
+                                                                          articles={articles}/>;
 
-//     describe('without props', () => {
-//         beforeEach(() => {
-//             sut = render(props, children)
-//         });
+    describe('without props', () => {
+        beforeEach(() => {
+            sut = render(props, children)
+        });
 
-//         it('should match snapshot', () => {
-//             expect(sut).toMatchSnapshot();
-//         })
-//     });
+        it('should match snapshot', () => {
+            expect(sut).toMatchSnapshot();
+        })
+    });
 
-//     describe('when passed custom props', () => {
-//         beforeEach(() => {
-//             sut = render(props, children)
-//         });
+    describe('when passed custom props', () => {
+        beforeEach(() => {
+            sut = render(props, children)
+        });
 
-//         it('should return searchValue in Children component', () => {
-//             const { searchValue } = sut.find('Children').props();
+        it('should return searchValue in Children component', () => {
+            const { searchValue } = sut.find('Children').props();
 
-//             expect(searchValue).toBe('testValue')
-//         })
-//     })
-// })
+            expect(searchValue).toBe('testValue')
+        })
+    })
+})
